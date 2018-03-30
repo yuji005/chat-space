604b8bc7c6f7b57f71dc2fee9786951c26ba7f9d
yuji005 <yuji0816@gmail.com>
2018-03-30 21:48:02 +0900

    グループ機能

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180329113344) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
