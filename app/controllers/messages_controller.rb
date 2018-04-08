9d5e090bbb5626f1ced07ab83b0484f4cb0d8ee8
yuji005 <yuji0816@gmail.com>
2018-04-08 15:50:56 +0900

    修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,6 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    #binding.pry
     if @message.save
     respond_to do |format|
       format.html { redirect_to group_messages_path(@group)  }
